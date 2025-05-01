05e4a926690fa8cf7a765592ff807066e4d82846
Danyely Frank <danyelypires@gmail.com>
2025-05-01 11:26:09 -0400

    Limpando PDFs antigos antes de gerar novo contrato

--- curso_python/variaveis.py
+++ curso_python/variaveis.py
@@ -1,8 +1,21 @@
-faturamento = 1000
-custo = 500
-print("O faturamento =", faturamento)
-print("O custo =", custo)
-print("O Lucro =", faturamento - custo)
 #variaveis é um nome e não pode nome de comando do python 
 #o código executa de cima para baixo. Definir as variaveis primeiro e depois faz o comando, se não vai aparecer o erro.
 # "="- recebe
+#int= numeros inteiros, float= numeros com casas decimais; strings = textos; booleen = booleanos(true or false)
+
+faturamento = 1100
+custo = 600
+novas_vendas = 150000
+faturamento = novas_vendas + faturamento
+lucro = faturamento - custo
+
+mensagem = print ("O faturamento da loja é de"), faturamento
+
+print("O faturamento =", faturamento)
+print("O custo =", custo)
+print("O Lucro =", faturamento - custo)
+
+#operadores especiais
+#mod -> % - percentual - resto da divisão de um numero pelo outro 
+#10 % 3 - qual é o resto da divisão? 
+print(10 % 3)
